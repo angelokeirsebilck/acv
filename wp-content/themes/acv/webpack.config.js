dea6ff415a3f8a863f70ea4b072dfa7a4d8c1499
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2021-01-27 21:21:41 +0100

    Add browserlist, to hopefully fix react on android

--- wp-content/themes/acv/webpack.config.js
+++ wp-content/themes/acv/webpack.config.js
@@ -99,7 +99,10 @@ let config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        presets: [
+                            '@babel/preset-react',
+                            ['@babel/preset-env', { targets: { node: '12' } }],
+                        ],
                     },
                 },
             },
