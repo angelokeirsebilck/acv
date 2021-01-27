0eeb1cf3c407a3e734e3a92d7846a90a3f0ab64a
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2021-01-27 14:43:53 +0100

    Test again

--- wp-content/themes/acv/functions.php
+++ wp-content/themes/acv/functions.php
@@ -3,10 +3,9 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 // Load Carbon Fields
-add_action('after_setup_theme', 'crb_load');
-function crb_load()
-{
-    require_once('vendor/autoload.php');
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+    require_once( ABSPATH . '/vendor/autoload.php' );
     \Carbon_Fields\Carbon_Fields::boot();
 }
 
