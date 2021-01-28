a923961cef5f25ce59b30662ea3027c2ed2c4b3b
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2021-01-28 09:53:12 +0100

    test

--- wp-content/themes/acv/front-page.php
+++ wp-content/themes/acv/front-page.php
@@ -3,7 +3,9 @@
     <h1 class="invisible">Main</h1>
     <?php get_template_part('template-parts/home/homeBanner') ?>
     <?php get_template_part('template-parts/home/who') ?>
+    <div class="values"> 
 
-</main>
+    </div>
+</main> 
 
 <?php get_footer();
