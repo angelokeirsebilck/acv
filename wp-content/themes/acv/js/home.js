b3712dfd32a7e97eafe522ecfb96c37ef43ce587
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2021-01-28 09:55:04 +0100

    test

--- wp-content/themes/acv/js/home.js
+++ wp-content/themes/acv/js/home.js
@@ -1,9 +1,9 @@
 import './modules/sliderHome';
 
-const values = ['This', 'ES', 'Function', 'Got', 'Transpiled'];
+// const values = ['This', 'ES', 'Function', 'Got', 'Transpiled'];
 
-const valuesContainer = document.querySelector('.values');
+// const valuesContainer = document.querySelector('.values');
 
-values.map((item) => {
-    valuesContainer.innerHTML = valuesContainer.innerHTML + item;
-});
+// values.map((item) => {
+//     valuesContainer.innerHTML = valuesContainer.innerHTML + item;
+// });
